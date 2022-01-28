3ea85b5c02150b3455a9d2e0a4ccd5db738d44d7
Kim Nguyen <kimnivore@gmail.com>
2022-01-28 13:22:39 -0800

    completed testing

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -8,7 +8,7 @@ import Article from './Article';
 const sampleArticle = [
         {
             headline: "Less than half of Seattle homes have air conditioning. After a deadly heat wave, ‘everybody’ wants it.",
-            author:"",
+            author: '',
             image: 134,
             summary: "Triple-digit temperatures led to a spike in demand across the region.",
             body: "Inside the attic of a one-story gray house in a Seattle suburb last week, Jeff Bryson gingerly strapped copper piping across the rafters while wearing a white face mask and a headlamp. The temperature was about 110 degrees in the tight space, which was covered in insulation dust. His work was meant to cool the rest of the home."   
@@ -29,10 +29,19 @@ test('renders headline, author from the article when passed in through props', (
 });
 
 test('renders "Associated Press" when no author is given', () => {
+    render(<Article article={sampleArticle} />);
+    const altText = screen.queryByText(/associated press/i);
+    expect(altText).toBeInTheDocument();
 });
 
-test('executes handleDelete when the delete button is pressed', () => {
+test('executes handleDelete when the delete button is pressed', async () => {
+    const handleDelete = jest.fn();
+    render(<Article article={sampleArticle} handleDelete={handleDelete}/>);
+    const button = screen.queryByTestId('deleteButton');
+    userEvent.click(button);
+    expect(handleDelete).toBeCalled();
 });
+
 
 //Task List: 
 //1. Complete all above tests. Create test article data when needed.
