fd228ffdf2823f9cfd2ecbf8dfa25be183d9539f
Kim Nguyen <kimnivore@gmail.com>
2022-01-28 10:24:02 -0800

    setArticles on moount

--- src/components/View.js
+++ src/components/View.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import Article from './Article';
 import EditForm from './EditForm';
 
+import axiosWithAuth from '../utils/axiosWithAuth';
+
 const View = (props) => {
     const [articles, setArticles] = useState([]);
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('/articles')
+            .then(resp => {
+                console.log(resp);
+                setArticles(resp.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
 
     const handleDelete = (id) => {
     }
